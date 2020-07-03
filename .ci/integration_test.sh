a703f222058171e50f82f905fe2d4b766009c788
Andreas Burger <andreas.burger@sap.com>
2020-07-03 16:02:50 +0000

    install helm in new job image

--- .ci/integration_test.sh
+++ .ci/integration_test.sh
@@ -4,6 +4,9 @@ set -e
 
 repo_dir="$(readlink -f "$(dirname "${0}")/..")"
 
+# HACK: will fail at installation of python, which is already provided - we only need helm in this image
+make -C "${repo_dir}" install-requirements || true
+
 pip3 install -r "${repo_dir}/test/requirements.txt"
 
 python3 "${repo_dir}/.ci/integration_test.py" \
